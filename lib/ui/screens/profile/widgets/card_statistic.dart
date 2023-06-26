b49cec61159d2e1bfe462d5314aa9a3a3649b74e
HaiLT <hai.le.thanh@ofatechjsc.com>
2023-06-26 22:50:10 +0700

    Revert "optimize widget"
    This reverts commit cd74b092f6187112862e7e2ece7f470da82641d0.

--- lib/ui/screens/profile/widgets/card_statistic.dart
+++ lib/ui/screens/profile/widgets/card_statistic.dart
@@ -5,32 +5,34 @@ import 'package:my_app/core/base/base_widget_mixin.dart';
 import 'package:my_app/core/values/app_values.dart';
 import 'package:my_app/ui/widgets/spacer.dart';
 
-class CardStatistic extends StatelessWidget with BaseWidgetMixin {
-  final String icon;
-  final String content;
+class StatisticCard extends StatelessWidget with BaseWidgetMixin {
+  final String text;
+  final String iconName;
 
-  const CardStatistic({
-    required this.icon,
-    required this.content,
+  const StatisticCard({
+    required this.text,
+    required this.iconName,
   });
 
   @override
   Widget body(BuildContext context) {
     return Container(
+      height: 44,
       padding: const EdgeInsets.all(14),
       decoration: BoxDecoration(
-          color: AppColors.grey500, borderRadius: BorderRadius.circular(10)),
+          color: AppColors.grey500,
+          borderRadius: BorderRadius.circular(10)),
       child: Row(
         children: [
           SvgPicture.asset(
-            icon,
+            iconName,
             color: AppColors.darkGreen,
             width: AppValues.iconSize_20,
             height: AppValues.iconSize_20,
           ),
           const HSpacer(10),
           Text(
-            content,
+            text,
             style: const TextStyle(
                 color: AppColors.grey800, fontWeight: FontWeight.w400),
           )
